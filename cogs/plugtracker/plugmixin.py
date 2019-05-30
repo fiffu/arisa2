f1d5b33bc1c5405a9feb20defbc59522b48f7561
fiffu <fhejehfif@gmail.com>
2019-05-30 22:39:13 +0800

    plugmixin: Workaround parsedatetime NLP not recognising 'mins' as minutes (#8)

--- cogs/plugtracker/plugmixin.py
+++ cogs/plugtracker/plugmixin.py
@@ -66,6 +66,7 @@ class PlugPost(object):
     def timestamp(self) -> datetime.datetime:
         def parse_time(time_str):
             for short, full in [
+                ('mins', 'min')
                 ('hr', 'hour'),
                 ('wk', 'week'),
                 ('yr', 'year'),

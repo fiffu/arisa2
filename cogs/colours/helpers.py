42d7936589d1b48d66665a50ecc7bb77e4b07632
fiffu <fhejehfif@gmail.com>
2019-07-14 16:43:39 +0800

    colours: Re-implement mutate; cleanup helpers

--- cogs/colours/helpers.py
+++ cogs/colours/helpers.py
@@ -1,50 +1,11 @@
-from colorsys import rgb_to_hsv, hsv_to_rgb
 import json
 import os
-from random import random, uniform
 
 from utils.snippets import getabsdir
 
 
 COL_NAME_CACHE = dict()
 COL_NAME_FILE = os.path.join(getabsdir(__file__), 'colours.json')
-
-
-def fiddle(n, max_dist=0.2, min_dist=0):
-    diff = uniform(min_dist, max_dist)
-    diff *= -(random() > 0.5) or 1
-    return n + diff
-
-
-def cap(n, floor=0, ceil=1):
-    if n < floor:
-        return floor
-    elif n > ceil:
-        return ceil
-    else:
-        return n
-
-
-def _mutate(h, s, v, repeats=1):
-    global fiddle, cap
-    # Bind locally to speedup
-    fid = fiddle
-    cap = cap
-    for _ in range(repeats):
-        h = abs(fid(h)) % 1
-        s = cap(fid(s))
-        v = cap(fid(v))
-    return h, s, v
-
-
-def mutate_hsv(h, s, v, repeats=1):
-    return _mutate(h, s, v, repeats)
-
-
-def mutate_rgb(r, g, b, repeats=1):
-    h, s, v = rgb_to_hsv(r, g, b)
-    h, s, v = _mutate(h, s, v, repeats)
-    return hsv_to_rgb(h, s, v)
 
 
 def to_hexcode(r, g, b) -> str:
@@ -62,3 +23,9 @@ def get_colour_name(r, g, b):
     if not COL_NAME_CACHE.get('setup'):
         init_name_cache()
     return COL_NAME_CACHE.get((r, g, b))
+
+
+def clamp(lowerbound, upperbound):
+    def _clamp(val):
+        return max(lowerbound, min(val, upperbound))
+    return _clamp

8b0d6444828ce70cd383f578647d51292e6c3a26
fiffu <fhejehfif@gmail.com>
2019-08-18 23:38:08 +0800

    colours: Shrink test values in config

--- cogs/colours/config.py
+++ cogs/colours/config.py
@@ -1,8 +1,9 @@
 MUTATE_COOLDOWN_TIME = dict(hours=4)
 REROLL_COOLDOWN_TIME = dict(hours=12)
 REROLL_PENALTY_TIME = dict(minutes=30)
-# MUTATE_COOLDOWN_TIME = dict(seconds=5)
-# REROLL_COOLDOWN_TIME = dict(seconds=30)
+# MUTATE_COOLDOWN_TIME = dict(seconds=1)
+# REROLL_COOLDOWN_TIME = dict(seconds=5)
+# REROLL_PENALTY_TIME = dict(seconds=1)
 
 DB_UPDATE_TIMEOUT_SECS = 10
 

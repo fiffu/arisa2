54c5aec21ffed18274505aa3d2dd01656519eeca
fiffu <fhejehfif@gmail.com>
2019-09-01 22:30:13 +0800

    cogs: Add missing super-init for DatabaseCogMixing subclasses

--- cogs/publishsubscribecog.py
+++ cogs/publishsubscribecog.py
@@ -24,6 +24,7 @@ class NotChannelError(ValueError):
 
 class PublishSubscribe(DatabaseCogMixin, commands.Cog):
     def __init__(self, bot):
+        super().__init__()
         self.bot = bot
         self.topics = defaultdict(list)
 

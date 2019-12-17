28f03a1e88b31a69fd9657f6696ef82ffbf660a7
fiffu <fhejehfif@gmail.com>
2019-12-18 00:07:28 +0800

    colours: Switch off debug values from config

--- cogs/colours/config.py
+++ cogs/colours/config.py
@@ -1,10 +1,10 @@
-# MUTATE_COOLDOWN_TIME = dict(hours=4)
-# REROLL_COOLDOWN_TIME = dict(hours=12)
-# REROLL_PENALTY_TIME = dict(minutes=30)
+MUTATE_COOLDOWN_TIME = dict(hours=4)
+REROLL_COOLDOWN_TIME = dict(hours=12)
+REROLL_PENALTY_TIME = dict(minutes=30)
 
-MUTATE_COOLDOWN_TIME = dict(seconds=1)
-REROLL_COOLDOWN_TIME = dict(seconds=5)
-REROLL_PENALTY_TIME = dict(seconds=1)
+# MUTATE_COOLDOWN_TIME = dict(seconds=1)
+# REROLL_COOLDOWN_TIME = dict(seconds=5)
+# REROLL_PENALTY_TIME = dict(seconds=1)
 
 DB_UPDATE_TIMEOUT_SECS = 10
 

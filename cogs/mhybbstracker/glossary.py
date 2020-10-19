1b973ad963405a55a4882b6611db46d846e7825f
fiffu <fhejehfif@gmail.com>
2020-10-19 19:33:51 +0800

    mhybbs: Expand glossary for Genshin Impact

--- cogs/mhybbstracker/glossary.py
+++ cogs/mhybbstracker/glossary.py
@@ -1,4 +1,30 @@
-TRANSLATION_GLOSSARY = {
-    '原神': 'Genshin',
-    '可莉': 'Klee',
-}
+# -*- coding: utf-8 -*-
+
+TRANSLATION_GLOSSARY = [
+    ('原神', 'Genshin'),
+    ('可莉', 'Klee'),
+    ('迪卢克', 'Diluc'),
+    ('安柏', 'Amber'),
+    ('班尼特', 'Bennett'),
+    ('香菱', 'Xiangling'),
+    ('莫娜', 'Mona'),
+    ('芭芭拉', 'Barbara'),
+    ('行秋', 'Xingqiu'),
+    ('琴', 'Jean'),
+    ('温迪', 'Venti'),
+    ('魈', 'Xiao'),
+    ('旅行者', 'Traveler'),
+    ('砂糖', 'Sucrose'),
+    ('刻晴', 'Keqing'),
+    ('北斗', 'Beidou'),
+    ('丽莎', 'Lisa'),
+    ('雷泽', 'Razor'),
+    ('菲谢尔', 'Fischl'),
+    ('七七', 'Qiqi'),
+    ('凯亚', 'Kaeya'),
+    ('冲云', 'Chongyun'),
+    ('诺艾尔', 'Noelle'),
+    ('凝光', 'Ningguang'),
+    ('神里绫华', 'Kamisato Ayaka'),
+    ('绫华', 'Ayaka'),
+]
--- translation/translation.py
+++ translation/translation.py
@@ -5,29 +5,12 @@ TODO - make async: either wrap in loop.run_in_executor() or fork and use httpx
 from googletrans import Translator
 
 
-rd = {
-    '原神': 'Genshin',
-    '可莉': 'Klee',
-}
-
 client = Translator()
 
 def translate(text, src='zh-cn', dest='en', replacements=None):
     if replacements:
-        for k, v in replacements.items():
+        for k, v in replacements:
             text = text.replace(k, v)
 
     result = client.translate(text, src=src, dest=dest)
     return result.text
-
-
-def main():
-    s = """
-    《原神》角色演示
-
-    「可莉：哒哒哒」
-    """
-    print(translate(s, replacements=rd))
-
-if __name__ == '__main__':
-    main()
